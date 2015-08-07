85e014582850e6efaecced8680686eddfa0b6ced
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-08-07 16:47:12 +1000

    Added first model definitions in ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,55 +11,40 @@
 |
 */
 
-$factory->define(App\User::class, function ($faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
-});
-
-$factory->define(App\Subreddit::class, function ($faker) {
-    return [
-        'name'  => $faker->company,
-        'description' => $faker->catchPhrase
-    ];
-});
-
-$factory->define(App\Post::class, function ($faker) {
-    return [
-        'title' => $faker->sentence(5),
-        'content' => $faker->sentence(15),
-        'score' => $faker->randomNumber(4),
-        'user_id' => App\User::orderByRaw('RAND()')->first()->id,
-        'subreddit_id' => App\Subreddit::orderByRaw('RAND()')->first()->id
-    ];
-});
-
-$factory->define(App\Comment::class, function ($faker) {
-    if (App\Comment::count()) {
-        $model_class = rand(0,1) ? App\Post::class : App\Comment::class;
-    } else {
-        $model_class = App\Post::class;
+$factory->define(
+    App\User::class, function ($faker) {
+        return [
+            'name' => $faker->name,
+            'email' => $faker->email,
+            'password' => bcrypt(str_random(10)),
+            'remember_token' => str_random(10),
+        ];
     }
-    $model_instance = new $model_class;
-    return [
-        'content' => $faker->sentence(15),
-        'score' => $faker->randomNumber(4),
-        'user_id' => App\User::orderByRaw('RAND()')->first()->id,
-        'commentable_type' => $model_class,
-        'commentable_id' => $model_instance->newQuery()->orderByRaw('RAND()')->first()->id
-    ];
-});
+);
 
-$factory->define(App\Vote::class , function ($faker) {
-    $model_class = rand(0,1) ? App\Post::class : App\Comment::class;
-    $model_instance = new $model_class;
-    return [
-        'dir' => rand(0,1) ? -1 : 1,
-        'user_id' => App\User::orderByRaw('RAND()')->first()->id,
-        'commentable_type' => $model_class,
-        'commentable_id' => $model_instance->newQuery()->orderByRaw('RAND()')->first()->id
-    ];
-});
+$factory->define(
+    App\Role::class, function ($faker) {
+        return [
+            'name' => $faker->name,
+        ];
+    }
+);
+
+$factory->define(
+    App\Subject::class, function ($faker) {
+        return [
+            'code' => str_random(10),
+            'name' => $faker->name
+        ];
+    }
+);
+
+$factory->define(
+    App\Message::class, function ($faker) {
+        return [
+            'from_user' => -1,
+            'to_user' => -1,
+            'message' => ''
+        ];
+    }
+);
