4d7e80991ab766798762bac26f156342e1fa721b
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-08-11 10:11:21 +1000

    refactoring

--- resources/views/vote/create.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ $action }}" method="post">
-	{!! csrf_field() !!}
-	<div class="btn-group-vertical">
-		<button class="btn btn-default btn-xs glyphicon glyphicon-chevron-up" type="submit" name="direction" value="1"></button>
-		<a class="btn btn-default disabled">{{ $score or ''}}</a>
-		<button class="btn btn-default btn-xs glyphicon glyphicon-chevron-down" type="submit" name="direction" value="-1"></button>
-	</div>
-</form>
