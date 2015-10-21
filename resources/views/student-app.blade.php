c5422a2f54e917424c7ef80742a5cdb5c28dde53
Ben Nicholson <bnic7531@uni.sydney.edu.au>
2015-10-21 20:32:31 +1100

    must click button for message modal to appear

--- resources/views/student-app.blade.php
+++ resources/views/student-app.blade.php
@@ -32,12 +32,12 @@
                     <ul class="list-group">
                         @foreach ($tutors as $tutor)
                             <li class="list-group-item"
-                                data-toggle="modal"
-                                data-target=".send-message-to-{{ $tutor->id }}">{{ $tutor->name }}
+                                >{{ $tutor->name }}
                                 <span style="position:absolute; right:10px;top:3px;">
                                     <span class="label label-primary">Rating: {{ $tutor->rating }}</span>
                                     <a href="/up-rating?tutor_id={{$tutor->id}}"><button class="btn btn-success"><img style="width:20px;height:auto;" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-up-b-128.png"></button></a>
                                     <a href="/down-rating?tutor_id={{$tutor->id}}"><button class="btn btn-danger"><img style="width:20px;height:auto;"src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-128.png"></button></a>
+                                    <button class="btn btn-info" data-toggle="modal" data-target=".send-message-to-{{ $tutor->id }}">Send Message</button>
                                 </span>
                             </li>
                             @include('message.send', ['to_user' => $tutor->id])
