19f66cd09c2607de526f43609438341d68405755
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-10-23 10:54:36 +1100

    added profile view to home tutor page

--- resources/views/tutor-app.blade.php
+++ resources/views/tutor-app.blade.php
@@ -45,8 +45,32 @@
                 @endforeach
             </ul>
         </div>
-        <div>
-            <h2><span class="label label-primary">My Rating: {{ Auth::user()->score }}</span></h2>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    My Profile
+                    <span class="label label-primary pull-right">My Rating: {{ Auth::user()->score }}</span>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <form action="/profile" method="post">
+                    @foreach ($errors->all() as $e)
+                        <div class="alert alert-danger" role="alert">{{ $e }}</div>
+                    @endforeach
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+                        <label>$ Rate / Hour</label>
+                        <input class="form-control" type="number" name="rate" placeholder="$rate / hour" value="{{Auth::user()->rate}}">
+                    </div>
+                    <div class="form-group">
+                        <label>Blurb</label>
+                        <textarea class="form-control" name="blurb" placeholder="Tell us something about yourself!">{{ Auth::user()->blurb }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control btn-primary" type="submit" value="Update My Profile">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
     <div class="col-xs-6">
